15845b485339986417a7de0fecd037336d5c9434
Sasa Stamenkovic <umpirsky@gmail.com>
2012-02-18 12:08:51 +0100

    Added web.

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+* This file is part of the Simplex-Website.
+*
+* (c) Саша Стаменковић <umpirsky@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+require_once __DIR__ . '/../src/autoload.php';
+$app = require __DIR__ . '/../src/app.php';
+$app->run();
