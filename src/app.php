dd465d13e9685e4ba64bfb03311fbb945da03db3
Sasa Stamenkovic <umpirsky@gmail.com>
2012-02-19 17:34:11 +0100

    Moved navigation and page generation to service providers.

--- src/app.php
+++ src/app.php
@@ -1,22 +1,18 @@
 <?php
 
 /*
-* This file is part of the Simplex-Website.
-*
-* (c) Саша Стаменковић <umpirsky@gmail.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of the Simplex-Website.
+ *
+ * (c) Саша Стаменковић <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 require_once __DIR__ . '/../vendor/simplex/autoload.php';
 
-$config = \Symfony\Component\Yaml\Yaml::parse(__DIR__ . '/../config/app.yml');
-$config['twig.path'] = __DIR__ . '/../' . $config['twig.path']; // TODO: add application path as parameter in Simplex\Application
-
-$app = new Simplex\Application($config);
 $navigation = \Symfony\Component\Yaml\Yaml::parse(__DIR__ . '/../config/navigation.yml');
-$app->addNavigation($navigation);
-$app->addPages($navigation);
-
-return $app;
+return new Simplex\Application(array_merge(
+    array('navigation' => $navigation),
+    \Symfony\Component\Yaml\Yaml::parse(__DIR__ . '/../config/app.yml')
+));
